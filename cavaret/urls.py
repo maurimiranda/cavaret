07605d43dc5f654aed81e5c8bbf8dd0234c6ac1b
Mauricio Miranda <maurimiranda@gmail.com>
2020-05-18 11:35:02 -0300

    set order app routes as default

--- cavaret/urls.py
+++ cavaret/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('orders.urls')),
     path('orders/', include('orders.urls')),
     path('admin/', admin.site.urls),
 ]
