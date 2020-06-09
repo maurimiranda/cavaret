e330fab9df3bcc46b46c4e9796b1888e32579471
Mauricio Miranda <maurimiranda@gmail.com>
2020-06-09 15:08:44 -0300

    Add pending orders

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 
-from .models import Order
+from .models import Order, Item
 
 # Create your views here.
 
 @login_required
 def index(request):
-    orders = Order.objects.filter(status__name='Ready')
-    return render(request, 'orders/index.html', {'orders': orders})
+    pending_items = Item.objects.filter(order__status='1')\
+        .values('product__provider__name', 'product__brand__name', 'product__name')\
+        .annotate(quantity=Sum('quantity'))\
+        .annotate(cost=Sum('cost'))
+
+    providers = {}
+
+    for item in pending_items:
+        provider = item['product__provider__name']
+        if provider not in providers:
+            providers[provider] = {
+                'total': 0,
+                'items': []
+            }
+
+        provider = providers[provider]
+        provider['total'] += item['cost']
+        provider['items'].append(item)
+
+    return render(request, 'orders/index.html', {
+        'pending_items': providers,
+        'ready_orders': Order.objects.filter(status__name='Ready')
+    })
